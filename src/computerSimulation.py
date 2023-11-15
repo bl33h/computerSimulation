67b68f44672047f5c8163c6ee768229897b2bbc1
bl33h <per21371@uvg.edu.gt>
2023-11-15 02:19:23 -0600

    bugs fixed

--- src/computerSimulation.py
+++ src/computerSimulation.py
@@ -3,25 +3,28 @@
 #Author: Sara Echeverria, Melissa Perez, Alejandro Ortega
 #Version: I
 #Creation: 14/11/2023
-#Last modification: 14/11/2023
+#Last modification: 15/11/2023
 
 import time
 import random
 
 def sumIntegers(a, b):
-    return a + b
+    return a / b
 
 # number of repetitions
-N = 100000
+N = int(10E5)
 
 # initialize total time
 totalTime = 0
 
+# store individual times for each operation
+operationTimes = []
+
 # simulation and time measurement
 for _ in range(N):
     # generate random values for a and b (ints)
-    a = random.randint(1000000000, 9000000000)
-    b = random.randint(1000000000, 9000000000)
+    a = random.randint(10000, 99999)
+    b = random.randint(10000, 99999)
 
     startTime = time.time()
 
@@ -33,9 +36,12 @@ for _ in range(N):
     # time accumulation
     totalTime += endTime - startTime
 
+    # store individual time for the operation
+    operationTimes.append(endTime - startTime)
+
 # calculate average time
 averageTime = totalTime / N
 
-print(f"addition result: {result}")
+print(f"division result: {result}")
 print(f"total time: {totalTime} seconds")
 print(f"average time per operation: {averageTime} seconds")
